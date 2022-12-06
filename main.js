aa222a03483d9c6344dafb24de55e27112d85428
e1pmiS <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@github.com>
2022-12-06 16:48:20 +0100

    from mac

--- main.js
+++ main.js
@@ -8,27 +8,62 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(bodyParser.raw());
+let BUSY = false;
+const Cell = class Cell {
+    constructor() {
+        this.id;
+        this.frequency;
+        this.type = "FDD";
+        this.rxPowerLevel;
+        this.mcc = "N/A";
+        this.mnc = "N/A";
+        this.decodedID = "N/A";
+        this.mib = {
+            Antennas: "nan",
+            RBs: "nan",
+            FN: "nan",
+            PHICH_duration: "nan",
+            Ng: "nan",
+        };
+        this.PDSCH = {
+            RNTI: "nan",
+            Modulation: "nan",
+            Redundancy_Version: "nan",
+            TransportBlock: null,
+        };
+    }
+};
 
 router.get("/", async function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 router.get("/getCells", async function (req, res) {
-  res.send(scanner.cells);
+    res.send(scanner.cells);
 });
 router.get("/reset", async function (req, res) {
-  scanner.cells = [];
-  scanner.frequencies = [];
+    scanner.cells = [];
+    scanner.frequencies = [];
 });
 
 router.get("/script.js", async function (req, res) {
-  res.sendFile("/home/ibra/kimo/java/script.js");
+    res.sendFile("script.js");
 });
 
 router.post("/submit", async function requestHandler(req, res) {
-  console.log("submit");
-  console.log(req.body);
-  await scanner.search(req.body.sFreq);
+    if (!BUSY) {
+        BUSY = true;
+        for (let i = 0; i < 3; i++) {
+            let cell = new Cell();
+            scanner.cells.push(cell)
+        }
+        console.log(scanner.cells)
+        console.log("submit");
+        console.log(req.body);
+    } else {
+        console.log("the server is busy");
+    }
+    // await scanner.search(req.body.sFreq);
 });
 // router.get('/sitemap',function(req,res){
 //   res.sendFile(path.join(__dirname+'/sitemap.html'));
--- script.js
+++ script.js
@@ -1,77 +1,75 @@
 const Body = document.body;
 let cells = [];
 let check = () => {
-  setTimeout(() => {
-    fetch("./getCells")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (myJson) {
-        if (myJson.length !== cells.length) {
-          const button = document.getElementById("gcells");
-          button.style.color = "red";
-          cells = myJson;
-        }
-        check();
-      })
-      .catch(function (error) {
-        console.log("Error: " + error);
-      });
-  }, 1000);
+    setTimeout(() => {
+        fetch("./getCells")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                if (myJson.length !== cells.length) {
+                    const button = document.getElementById("gcells");
+                    button.style.color = "red";
+                    cells = myJson;
+                }
+                check();
+            })
+            .catch(function (error) {
+                console.log("Error: " + error);
+            });
+    }, 1000);
 };
 check();
 
 let reset = (document.getElementById("reset").onclick = () => {
-  cells = [];
-  fetch("./reset")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {})
-    .catch(function (error) {
-      console.log("Error: " + error);
-    });
+    cells = [];
+    fetch("./reset")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {})
+        .catch(function (error) {
+            console.log("Error: " + error);
+        });
 });
 
 let getCells = (document.getElementById("gcells").onclick = () => {
-  const button = document.getElementById("gcells");
-  button.style.color = "black";
-  fetch("./getCells")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      console.log(myJson.length);
-      try {
-        const table = document.getElementById("table");
-        let tableRows = table.getElementsByTagName("tr");
-        let rowCount = tableRows.length;
+    const button = document.getElementById("gcells");
+    button.style.color = "black";
+    fetch("./getCells")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log(myJson.length);
+            try {
+                const table = document.getElementById("table");
+                let tableRows = table.getElementsByTagName("tr");
+                let rowCount = tableRows.length;
 
-        for (let x = rowCount - 1; x > 0; x--) {
-          table.removeChild(tableRows[x]);
-        }
-      } catch {
-        
-      }
-      let x = myJson.length % 3;
-      let y = myJson.length + (3 - x);
-      let rawsnumber = y / 3;
-      if (myJson.length <= 3) {
-        rawsnumber = 1;
-      }
-      console.log(x + y + rawsnumber);
-      for (let i = 0; i < rawsnumber; i++) {
-        let row = table.insertRow();
-        for (let i = 0; i < 3; i++) {
-          if (myJson.length > 0) {
-            let cell = myJson.pop();
-            let TableCell = row.insertCell();
-            TableCell.innerHTML = `<ul class='ul li ol'> <li>Cell id: ${cell.id} </li> <li>Operting frequency: ${cell.frequency}</li><li>Cell type: ${cell.type}</li><li>Rx power level: ${cell.rxPowerLevel}</li><li>MCC: ${cell.mcc}</li><li>MNC: ${cell.mnc}</li></ul> <p><a role='button'>BCCH</a></p>`;
-          }
-        }
-      }
-    })
-    .catch(function (error) {
-      console.log("Error: " + error);
-    });
+                for (let x = rowCount - 1; x > 0; x--) {
+                    table.removeChild(tableRows[x]);
+                }
+            } catch {}
+            let x = myJson.length % 3;
+            let y = myJson.length + (3 - x);
+            let rawsnumber = y / 3;
+            if (myJson.length <= 3) {
+                rawsnumber = 1;
+            }
+            console.log(x + y + rawsnumber);
+            for (let i = 0; i < rawsnumber; i++) {
+                let row = table.insertRow();
+                for (let i = 0; i < 3; i++) {
+                    if (myJson.length > 0) {
+                        let cell = myJson.pop();
+                        let TableCell = row.insertCell();
+                        TableCell.innerHTML = `<ul class='ul li ol'> <li>Cell id: ${cell.id} </li> <li>Operting frequency: ${cell.frequency}</li><li>Cell type: ${cell.type}</li><li>Rx power level: ${cell.rxPowerLevel}</li><li>MCC: ${cell.mcc}</li><li>MNC: ${cell.mnc}</li></ul> <p><a role='button'>BCCH</a></p>`;
+                    }
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log("Error: " + error);
+        });
 });
